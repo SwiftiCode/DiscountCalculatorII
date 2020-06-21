0dd74eea9aae3509cdbd397d603737f272ceddfb
SwiftCode <SwiftCode@gamil.com>
2020-06-21 16:09:19 +0800

    Initial Commit

---
+++ DiscountCalculatorII/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DiscountCalculatorII
+//
+//  Created by SwiftiCode on 21/6/20.
+//  Copyright © 2020 SwiftiCode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
